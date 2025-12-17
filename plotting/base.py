387b87b8e80eed032a873612908294babdda2097
ejahr <ejahr@protonmail.com>
2025-12-17 10:24:18 +0100

    draws gridlines below data

--- plotting/base.py
+++ plotting/base.py
@@ -7,3 +7,4 @@ def set_rcParams():
     plt.rcParams['mathtext.fontset'] = 'stix'
     plt.rcParams['font.family'] = 'STIXGeneral'
     plt.rcParams.update({'font.size': 16})
+    plt.rcParams['axes.axisbelow'] = True
