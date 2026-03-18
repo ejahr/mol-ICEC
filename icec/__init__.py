5f61e0f42d7a36e37ddab4390ba456463c1edb26
ejahr <ejahr@protonmail.com>
2026-03-18 14:49:06 +0100

    proper __init__.py file for icec module

---
+++ icec/__init__.py
@@ -0,0 +1,8 @@
+# Define the __all__ variable
+__all__ = ["constants", "icec", "intraIcec", "morse"]
+
+# Import the submodules
+from . import constants
+from . import icec
+from . import intraIcec
+from . import morse
