5111b3ec79b400409b94f3624a8235e449658459
ejahr <ejahr@protonmail.com>
2026-05-18 18:34:05 +0200

    start moving parameters to config file

--- H-LiH.py
+++ H-LiH.py
@@ -26,34 +26,20 @@ class Hp_LiH():
         R_min = r_LiH - r_COM_LiH + r_HH
         return R_min
 
-# ===== Define which parts are active =====
-calc_roots      = 0
-bb              = 1
-bc              = 0
-FC              = 1
-plotting        = 0
-calculate       = 1
-spectra         = 1
-cross_section   = 0
-temp_dependence = 0
-plot_info       = 1
-print_info      = 0
+electronE   = config.electronE
+R           = Hp_LiH.R_min() # 6*Units.ANGSTROM2BOHR TODO change to config
 
-electronE   = 1*Units.EV2HARTREE
-R           = Hp_LiH.R_min() # 6*Units.ANGSTROM2BOHR
-L           = 8*Units.ANGSTROM2BOHR
-T           = [15, 300, 1500] 
-
-vD_max_bc   = 7
-min_kinE    = 0.01 * Units.EV2HARTREE
-max_kinE    = 9 * Units.EV2HARTREE
-max_dissE   = 2 * Units.EV2HARTREE
+vD_max_bc   = config.vD_max_bc
+min_kinE    = config.min_kinE
+max_kinE    = config.max_kinE
+max_dissE   = config.max_dissE
 max_dissE_1 = 1 * Units.EV2HARTREE
-num_grid    = 1000
-n_max       = 10     # rydberg states
+num_grid    = config.num_grid
+n_max       = config.n_max     # rydberg states
 
-system = 'Hp-LiH'
-title = r'$\text{H}^+ \text{LiH}$'
+system      = config.system
+header      = config.reaction + '\n'
+title       = config.title
 
 # ===== Define ICEC classes for calculating ICEC cross sections =====
 
@@ -80,9 +66,9 @@ icec_FC.make_energy_grid(min_kinE, maxEnergy=4.5*Units.EV2HARTREE, num=num_grid)
 icec_FC.define_PI_xs_D(method="FC")
 
 # --- calculate or load dissociative energies ---
-icec_FC.Morse_Dp.define_box(L)
-fname = DIR_DATA + f'LiH/LiHp.diss_energies.E{round(max_dissE*Units.HARTREE2EV,1)}eV.L{round(L*Units.BOHR2ANGSTROM)}A.txt'
-if calc_roots:
+icec_FC.Morse_Dp.define_box(config.L)
+fname = config.DIR_DATA + f'LiH/LiHp.diss_energies.E{round(max_dissE*Units.HARTREE2EV,1)}eV.L{round(config.L*Units.BOHR2ANGSTROM)}A.txt'
+if config.calc_roots:
     roots, root_estimates = Morse.save_diss_states(fname, max_dissE, num=1000)
     plot.pes.plot_roots(icec_FC.Morse_Dp, roots, root_estimates, max_dissE)
     
@@ -94,54 +80,38 @@ IP_vertical = icec_el.IP_B + (icec.Morse_Dp.V(icec.Morse_D.re) + icec.Morse_Dp.D
 icec_el.IP_B = IP_vertical
 icec_el.make_energy_grid(min_kinE, LiH.max_kinE_unresolved, num_grid)
 
-if print_info:
-    print('\n===== Info =====')
-    print(f"vertical ionization energy {round(IP_vertical*Units.HARTREE2EV,3)} eV")
-    print(f"   approx                  {round(LiH.IP_vert_approx*Units.HARTREE2EV,3)} eV")
-    print(f"adiabatic ionizaton energy {round(IP_adiabatic*Units.HARTREE2EV,3)} eV")
-    print(f"   approx min to min       {round(LiH.IP_min_approx*Units.HARTREE2EV,3)} eV")
-    print(f"energy diff at R=inf       {round(LiH.energy_diff_at_inf*Units.HARTREE2EV,3)} eV")
-    #print_FC_factor(icec_FC, 0, 1.3*Units.EV2HARTREE)
-    test_FC_factors(icec_el, icec, icec_FC, R)
-    print_boltzmann_probabilities(icec_FC, T)
-    print_PI_crosssection(icec_FC)
-    
-    plot.cross_section.calculate_ratio_tot_vs_electronic(system, icec_el, R) 
 
-system = 'Hp-LiH'
-header = 'e- + H+ + LiH -> H + LiH+ + e-\n'
-
-if calculate:
-    if bb:
-        if cross_section:
-            #calc.cross_section.xs_bb(system, header, icec, R, LiH.v_max, LiHp.v_max)
+if config.calculate:
+    if config.bb:
+        if config.cross_section:
+            calc.cross_section.xs_bb(system, header, icec, R, LiH.v_max, LiHp.v_max)
             calc.cross_section.xs_rydberg_bb(system, header, icec_rydberg, R, n_max, 0, LiHp.v_max)
-            #if FC:
-                #calc.cross_section.xs_bb(system, header, icec_FC, R, modifier='-FC')
-        if spectra:      
+            if config.FC:
+                calc.cross_section.xs_bb(system, header, icec_FC, R, modifier='-FC')
+        if config.spectra:      
             calc.spectrum.spectrum_bb(system, header, icec_el, icec, R, electronE, LiH.v_max, LiHp.v_max)
-            if FC:
+            if config.FC:
                 calc.spectrum.spectrum_bb(system, header, icec_el, icec_FC, R, electronE, modifier='-FC')
 
-    if bc:
-        if cross_section:
+    if config.bc:
+        if config.cross_section:
             calc.cross_section.xs_bc(system, header, icec_FC, R, vD_max=0, max_dissE=max_dissE_1, modifier='-FC')
-        if spectra:
+        if config.spectra:
             calc.spectrum.spectrum_bc(system, header, icec_FC, R, electronE, vD_max_bc, modifier='-FC')
 
-if plotting:
-    if bb:
-        if cross_section and FC:
+if config.plotting:
+    if config.bb:
+        if config.cross_section and config.FC:
             plot.cross_section.xs_FC_bb(system, icec, R, icec_el)
-        if spectra and FC:
+        if config.spectra and config.FC:
             plot.spectrum.spectrum_FC_bb(system, R, electronE, LiH.v_max, icec_el=icec_el)
     
-    if bc:
-        if cross_section:
+    if config.bc:
+        if config.cross_section:
             plot.cross_section.xs_FC(system, icec_FC, R, icec_el)
-        if spectra:   
+        if config.spectra:   
             plot.spectrum.spectrum_FC(system, icec_FC, R, electronE, vD=0, icec_el=icec_el, secax_label=r"$E_{\mathrm{LiH}^+}$ [eV]")
-    #if cross_section and temp_dependence:
-    #    cross_sections.plot_xs_boltzmann_FC(system, icec_FC, R, T, vD_max_bc, icec_el=icec_el)
-    if spectra and temp_dependence:
-        plot.spectrum.boltzmann_FC(system, icec_FC, R, electronE, T, vD_max_bc)
+    #if config.cross_section and config.temp_dependence:
+    #    config.cross_sections.plot_xs_boltzmann_FC(system, icec_FC, R, config.T, vD_max_bc, icec_el=icec_el)
+    if config.spectra and config.temp_dependence:
+        plot.spectrum.boltzmann_FC(system, icec_FC, R, electronE, config.T, vD_max_bc)
--- config.py
+++ config.py
@@ -1,10 +1,43 @@
 import os
+from icec.constants import Units
+
 # get local DIR to keep file structure consistent
 DIR = os.path.dirname(os.path.realpath(__file__)) + '/'
+DIR_DATA = DIR + 'HLiH/data/'
+DIR_RESULTS = DIR + 'HLiH/results/'
+DIR_PLOTS = DIR + 'HLiH/plots/'
 
-DIR_DATA = os.path.dirname(os.path.realpath(__file__)) + '/HLiH/data/'
-DIR_RESULTS = os.path.dirname(os.path.realpath(__file__)) + '/HLiH/results/'
-DIR_PLOTS = os.path.dirname(os.path.realpath(__file__)) + '/HLiH/plots/'
+# === System ===
+unitA       = 'H'         # A-
+unitD       = 'LiH'
+system      = 'Hp-LiH'
+title       = r'$\text{H}^+ \text{LiH}$'            # plot title
+reaction    = 'e- + H+ + LiH -> H + LiH+ + e-'
 
-unitA = 'H+'
-unitD = 'LiH'
+# === Parameters ===
+R           = 3.95  * Units.ANGSTROM2BOHR
+L           = 8     * Units.ANGSTROM2BOHR
+
+vD_max_bc   = 7                             # maximum initial vibrational state considered for bound-continuum transitions
+min_kinE    = 0.01  * Units.EV2HARTREE      
+max_kinE    = 9     * Units.EV2HARTREE
+max_dissE   = 2     * Units.EV2HARTREE
+max_dissE_1 = 1     * Units.EV2HARTREE
+num_grid    = 1000
+n_max       = 10     # number of rydberg states
+
+electronE   = 1     * Units.EV2HARTREE      # incoming electron energy for spectrum
+T           = [15, 300, 1500]   # Kelvin    # temperatures for temperature dependent spectra
+
+# === Define which calculations are active ===
+calc_roots      = 0
+bb              = 1
+bc              = 0
+FC              = 1
+plotting        = 0
+calculate       = 1
+spectra         = 1
+cross_section   = 0
+temp_dependence = 0
+plot_info       = 1
+print_info      = 0
