e531d378d9fc19228f4826766840e974bd8ab6e8
ejahr <ejahr@protonmail.com>
2026-05-20 12:24:56 +0200

    imports fit as well

--- calc/__init__.py
+++ calc/__init__.py
@@ -1,6 +1,7 @@
 # Define the __all__ variable
-__all__ = ["cross_section", "spectrum"]
+__all__ = ["cross_section", "spectrum", "fit"]
 
 # Import the submodules
 from . import cross_section
 from . import spectrum
+from . import fit
